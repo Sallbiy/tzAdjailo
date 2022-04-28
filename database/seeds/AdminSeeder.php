1efdfe84292cc0fc8a499cbbad55402877b23ea4
Сальбий <s.barcho007@gmail.com>
2022-04-28 19:41:44 +0300

    -fix adminSeeder, add Hash

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class AdminSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'manager',
             'email' => 'manager@mail.ru',
-            'password' => '12345678',
+            'password' => Hash::make('12345678'),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
